96d43879e0d1835446f7cf510f572049d45ef10d
James Gobert <james.gobert@gmail.com>
2023-03-07 19:08:00 -0800

    Remove chatCommand

--- Sources/OpenAISwift/Models/Command.swift
+++ Sources/OpenAISwift/Models/Command.swift
@@ -18,16 +18,3 @@ struct Command: Encodable {
     }
 }
 
-struct ChatCommand: Encodable {
-     let messages: [Message]
-     let model: String
-     let maxTokens: Int
-     let temperature: Double
-
-     enum CodingKeys: String, CodingKey {
-         case messages
-         case model
-         case maxTokens = "max_tokens"
-         case temperature
-     }
- }
